4e1b20c6474fefcc50a96d22755db47f9d09164b
lucas <lucasff15@hotmail.com>
2023-02-01 18:44:28 -0300

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/Counter.js 	new file:   src/Header.js 	new file:   src/Menu.js 	modified:   src/index.js 	deleted:    src/logo.svg

---
+++ src/Counter.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+function Counter(props) {
+
+    const [count, setCount] = useState(() => parseInt(localStorage.getItem("count")) || props.count)
+
+    useEffect(() => {
+        document.title = count
+        localStorage.setItem("count", count)
+    }, [count])
+
+    function add(){
+        setCount(count + 1)
+    }
+
+    return (<div>
+        <h1>Counter: {count}</h1>
+        <button onClick={add}>Add</button>
+    </div>)
+}
+
+// class Counter extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {count: 0}
+//         this.add = this.add.bind(this);
+//     }
+    
+//     add() {
+//         // this.setState({ count: this.state.count +1})
+//         this.setState((state) => {return {count: state.count + 1}}, ()=>{
+//             console.log(this.state)
+//             localStorage.setItem("state", JSON.stringify(this.state))
+//         })
+//     }
+
+//     componentDidMount() {
+//         this.setState(JSON.parse(localStorage.getItem('state')))
+//     }
+    
+//     render() {
+//         return (<div>
+//             <h1>Counter: {this.state.count}</h1>
+//             <button onClick={this.add}>Add</button>
+//         </div>)
+//     }
+// }
+
+export default Counter;
+
